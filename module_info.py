26d66c985239dca0c01bc7c82eb882ffa80850d4
Cauê de Lima Varanda <caue.varanda@gmail.com>
2026-06-12 02:29:42 -0300

    Novo Diretorio
    Atualizado a pasta de diretorio para o mod correto

--- module_info.py
+++ module_info.py
@@ -2,7 +2,7 @@
 # Point export_dir to the folder you will be keeping your module
 # Make sure you use forward slashes (/) and NOT backward slashes (\)
 
-export_dir = "C:/Program Files (x86)/Steam/steamapps/common/MountBlade Warband/Modules/Montaria e Capacete/"
+export_dir = "C:/Program Files (x86)/Steam/steamapps/common/MountBlade Warband/Modules/Terra Brasilis/"
 #export_dir = "C:/Program Files (x86)/Mount&Blade Warband/Modules/Native/"
 
 load_mod_resource = "meus_capacetes"
